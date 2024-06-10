bb3688a78cfb2a6682466b22a0f1c20ae095135d
unknown <b11902156@ntu.edu.tw>
2024-06-10 22:40:20 +0800

    commit

--- start_game.py
+++ start_game.py
@@ -4,7 +4,6 @@ from agents.call_player import setup_ai as call_ai
 from agents.random_player import setup_ai as random_ai
 from agents.console_player import setup_ai as console_ai
 
-from src.agent import setup_ai as my_ai
 
 from baseline0 import setup_ai as baseline0_ai
 from baseline1 import setup_ai as baseline1_ai
@@ -17,10 +16,10 @@ from baseline7 import setup_ai as baseline7_ai
 
 config = setup_config(max_round=20, initial_stack=1000, small_blind_amount=5)
 config.register_player(name="p1", algorithm=baseline0_ai())
-config.register_player(name="p2", algorithm=my_ai())
+config.register_player(name="p2", algorithm=random_ai())
 
 ## Play in interactive mode if uncomment
 #config.register_player(name="me", algorithm=console_ai())
-game_result = start_poker(config, verbose=1)
+game_result = start_poker(config, verbose=2)
 
 print(json.dumps(game_result, indent=4))
