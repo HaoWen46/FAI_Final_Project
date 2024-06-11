d55f7c61cc4dd3a8c3d9487e4a41ab9921ef3b54
unknown <b11902156@ntu.edu.tw>
2024-06-11 23:03:54 +0800

    commit

--- train.py
+++ train.py
@@ -202,11 +202,11 @@ class Agent(object):
         with torch.no_grad():
             features = features.float().to(self.device)
             image = image.float().to(self.device)
-            print('hi')
             if use_target == True:
                 q_values = self.target_estimator(features=features, image=image).cpu().numpy()
             else:
                 q_values = self.estimator(features=features, image=image).cpu().numpy()
+            print('hi')
         return q_values
     
     def predict(self, features, image, legal_actions):
