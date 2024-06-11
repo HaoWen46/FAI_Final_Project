683fd14c6a7d5ba73d1b2aee5800399b465907ce
unknown <b11902156@ntu.edu.tw>
2024-06-11 19:15:40 +0800

    commit

--- train.py
+++ train.py
@@ -365,7 +365,7 @@ class Estimator(nn.Module):
         
         z = np.concatenate((x, y), axis=x.shape[-1])
         print(z.shape)
-        print(self.fc3.in_features)
+        print(self.fc3)
         z = nn.functional.elu(self.fc3(z))
         print('1')
         z = nn.functional.elu(self.fc4(z))
