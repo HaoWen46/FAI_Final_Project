444f01050f6ce0b9a3bdcc4be23f6b34cbc5de0a
unknown <b11902156@ntu.edu.tw>
2024-06-11 18:18:51 +0800

    commit

--- train.py
+++ train.py
@@ -71,9 +71,10 @@ class DDDQNPlayer(BasePokerPlayer):
     def __cards_to_image(self, hole_cards, community_cards):
         cards = hole_cards + community_cards
         images = np.zeros((8, 17, 17))
-        print(cards)
         for i in range(len(cards)):
-            images[i] = np.pad(np.zeros(4, 13), ((6, 7), (2, 2)), 'constant', constant_values=0)
+            a = np.zeros(4, 13)
+            images[i] = np.pad(a, ((6, 7), (2, 2)), 'constant', constant_values=0)
+            print('hi')
             images[i][SUITS.index(cards[i][0]) + 6][RANKS.index(cards[i][1]) + 2] = 1
         images[7] = images[:7].sum(axis=0)
         return np.swapaxes(images, 0, 2)[:, :, -1:]
