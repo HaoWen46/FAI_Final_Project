891617f2ad12fa9a76f628fb222ddd6239cdfe65
unknown <b11902156@ntu.edu.tw>
2024-06-12 10:57:55 +0800

    commit

--- train.py
+++ train.py
@@ -235,6 +235,7 @@ class Agent(object):
         
     def train(self):
         mini_batch = self.replay.sample(batch_size=self.batch_size)
+        print(mini_batch[0])
         feature_batch = torch.stack([f1 for (f1,i1,a,r,f2,i2,l,d) in mini_batch])
         image_batch = torch.stack([i1 for (f1,i1,a,r,f2,i2,l,d) in mini_batch])
         next_feature_batch = torch.stack([f2 for (f1,i1,a,r,f2,i2,l,d) in mini_batch])
