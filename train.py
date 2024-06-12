8ba96441741e4a8e8d6e5262fb2ca2ec7d81beba
unknown <b11902156@ntu.edu.tw>
2024-06-13 00:06:33 +0800

    commit

--- train.py
+++ train.py
@@ -443,5 +443,5 @@ def train(baselines, episodes=2500, lr=0.001, batch_size=128):
         for loss in losses:
             file.write(f'{loss}\n')
 
-baselines = [baseline0_ai]
+baselines = [random_ai]
 train(baselines=baselines)
