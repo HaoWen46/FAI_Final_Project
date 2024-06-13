e6d2ab700881031078a296eac825da9f9a07016e
unknown <b11902156@ntu.edu.tw>
2024-06-14 02:28:02 +0800

    commit

--- train.py
+++ train.py
@@ -443,6 +443,6 @@ def train(baselines, prob=None, episodes=5000, lr=0.001, batch_size=128):
             print(f'Winning rate: {total_wins / episode}')
 
 
-baselines = [baseline0_ai, baseline1_ai, baseline2_ai]
-prob = [0.3, 0.3, 0.4]
+baselines = [baseline1_ai, baseline2_ai, baseline3_ai, baseline4_ai]
+prob = [0.3, 0.3, 0.2, 0.2]
 train(baselines=baselines, prob=prob, episodes=5000)
