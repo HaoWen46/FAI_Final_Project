952090a4a2d74dc098622e919bbd5fb4454c0aef
unknown <b11902156@ntu.edu.tw>
2024-06-14 23:23:56 +0800

    commit

--- train.py
+++ train.py
@@ -447,6 +447,6 @@ def train(baselines, prob=None, episodes=5000, lr=0.001, batch_size=128):
     agent.save_checkpoint(filename=SAVE_PATH)
 
 
-baselines = [baseline3_ai, baseline4_ai, baseline5_ai, baseline6_ai, baseline7_ai, my_ai]
-prob = [0.1, 0.1, 0.15, 0.15, 0.15, 0.35]
+baselines = [baseline1_ai, baseline2_ai, baseline3_ai, baseline4_ai, baseline5_ai, baseline6_ai, baseline7_ai, my_ai]
+prob = [0.05, 0.05, 0.05, 0.1, 0.1, 0.15, 0.15, 0.35]
 train(baselines=baselines, prob=prob, episodes=500)
